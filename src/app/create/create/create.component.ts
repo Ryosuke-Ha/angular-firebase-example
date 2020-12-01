211a29ab8001f9148d19f623fc43bde74d7215a2
Ryosuke-Ha <ryosuke.0815@icloud.com>
2020-12-01 18:42:02 +0900

    feat(create):create画面追加

---
+++ src/app/create/create/create.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.scss']
+})
+export class CreateComponent implements OnInit {
+
+  form = this.formBuilder.group({
+    name:['',[
+      Validators.required,
+      Validators.maxLength(40)
+    ]],
+    gender:['',[
+      Validators.required,
+      Validators.pattern(/male|female/)
+    ]]
+  });
+
+  get nameControl(){
+    return this.form.get('name') as FormControl;
+  }
+
+  get genderControl(){
+    return this.form.get('gender') as FormControl;
+  }
+
+  constructor(
+    private formBuilder:FormBuilder,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  submit(){
+    console.log(this.form.value);
+  }
+
+}
